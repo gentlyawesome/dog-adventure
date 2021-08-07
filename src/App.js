942f702dceda26a499b03b358d44cfecdbd00657
Arc Angelo Ibalio <gentlyawesome@gmail.com>
2021-08-07 17:35:54 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import style from './App.module.css';
 
 function App() {
+
+  const [movement, setMovement] = useState('steady')
+  const [animal, setAnimal] = useState('dog')
+  const [timeoutId, setTimeoutId] = useState()
+
+  const keyMovements = [
+    { key: 87, animal: 'dog', movement: 'up'},
+    { key: 83, animal: 'dog', movement: 'down'},
+    { key: 65, animal: 'dog', movement: 'left'},
+    { key: 68, animal: 'dog', movement: 'right'},
+  ]
+
+  const checkMovementUp = (key) => {
+      const move = keyMovements.find(keyMovement => keyMovement.key === key.keyCode) 
+      setMovement( move !== undefined ? move.movement : 'steady')
+  }
+
+  useEffect(() => {
+    window.addEventListener('keyup', checkMovementUp)
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{movement}</h3>
+      <div className={`${style[animal]} ${style[movement]}`}></div>
     </div>
   );
 }
